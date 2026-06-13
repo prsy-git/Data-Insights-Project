dafe1e3f84c50d4adaec7f895a9860ab8f6ca757
prsy-git <ryan.pederson2601@gmail.com>
2026-06-13 12:44:13 -0700

    File upload routes
    Began writing app.py routes to handle file uploading and validation. Currently supports .csv for the simpler logic with plans to add additional file type support.

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
-from flask import Flask, render_template
+import os
+from flask import Flask, render_template, flash, request, redirect, url_for
+from werkzeug.utils import secure_filename
+
+UPLOAD_FOLDER = 'uploads/'
+ALLOWED_EXTENSIONS = {'csv'}
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+#webapp page routes
 
 @app.route('/')
 def home():
@@ -9,6 +17,31 @@ def home():
 @app.route('/dashboard')
 def dashboard():
     return render_template('dashboard.html')
+
+#file upload handling and routes
+def is_allowed_file(filename: str):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/dashboard', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        
+        file = request.files['file']
+
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        
+        if file and is_allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return render_template('dashboard.html')
+        
+    return render_template('dashboard.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
