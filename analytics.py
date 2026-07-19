433150fcca7e2c92759f2495fb7145337167efad
prsy-git <ryan.pederson2601@gmail.com>
2026-07-19 14:51:45 -0700

    Put filler text for landing page.
    Changed text on landing page to be edited later.

--- analytics.py
+++ analytics.py
@@ -61,6 +61,47 @@ def get_numeric_columns(df: pd.DataFrame) -> list:
 
     return numeric_cols
 
+def dataset_quality_alerts(df):
+    result = {
+        "warnings": [],
+        "confirmed_clear": "",
+        "outlier_details": {}
+    }
+
+    #If a specific column has a large amount of missing values (> 10%), throw an alert
+    missing_counts = df.isnull().sum()
+    ten_percent_missing = missing_counts[missing_counts > (len(df) * 0.1)]
+    
+    if not ten_percent_missing.empty:
+        cols = ", ".join(ten_percent_missing.index)
+        counts_warning = f"Alert: The columns {cols} are missing greater than 10% of their values. Cleaning the dataset with clean data button or external analysis is recommended."
+        result["warnings"].append(counts_warning)
+
+    #If ANY column contains outliers based on its quantile values, alert here. Have a separate dropdown in which specific values for specific columns can be viewed.
+    numeric_cols = get_numeric_columns(df)
+    affected_columns = []
+
+    for col in numeric_cols:
+        first_quantile = df[col].quantile(0.25)
+        third_quantile = df[col].quantile(0.75)
+        innerquartile_range = third_quantile - first_quantile
+
+        outliers = df[(df[col] < (first_quantile - 1.5 * innerquartile_range)) | (df[col] > (third_quantile + 1.5 * innerquartile_range))]
+        
+        if len(outliers) > 0:
+            affected_columns.append(col)
+            #Put column name and number of data points affected as a tuple entry in dictionary
+            result["outlier_details"][col] = len(outliers)
+
+    if affected_columns:
+        cols_str = ", ".join(affected_columns)
+        result["warnings"].append(f"🔍 Outliers detected in: [{cols_str}]. Open the dropdown below to review.")
+
+    if not result["warnings"]:
+        result["confirmed_clear"] = f"Dataset confirmed to not have any significant issues. There still may be some missing values, so clean if necessary"
+
+    return result
+
 #Returns a matplot axes value
 def graph_comparison(col1: pd.Series, col2: pd.Series, graph_type: str):
     #TODO: add cases allowing users to select multiple types of graphs, read in from form dropdown
@@ -88,7 +129,8 @@ def create_dashboard_data(passed_df:pd.DataFrame) -> dict:
         "col_count": count_columns(passed_df),
         "html_table": passed_df.head(50).to_html(),
         "col_list": passed_df.columns.tolist(),
-        "numeric_col_list": get_numeric_columns(passed_df)
+        "numeric_col_list": get_numeric_columns(passed_df),
+        "quality_report": dataset_quality_alerts(passed_df)
     }
 
     return analysis_dict
